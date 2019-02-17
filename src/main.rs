16ebdf9e72d913a90b791086ab24489c06add3ea
yutoVR <yuto.fushimi@icloud.com>
2019-02-17 23:37:44 +0900

    Added FizzBuzz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    fizzbuzz(20);
+}
+
+fn fizzbuzz(n: usize) {
+    for i in 1..n + 1 {
+        if i % 15 == 0 {
+            println!("FizzBuzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
 }
